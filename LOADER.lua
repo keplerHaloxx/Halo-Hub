5fd845efcfeff19ca9ae415e5e04f56006b68639
Haloxx <80098945+xvhHaloxx@users.noreply.github.com>
2023-01-20 00:54:30 +1100

    Update LOADER.lua

--- LOADER.lua
+++ LOADER.lua
@@ -1,3 +1,13 @@
 local Games = {
-  [11523066819] = "https://raw.githubusercontent.com/xvhHaloxx/Halo-Hub/main/Games/Tower%20Merge%20Simulator"
+  [11523066819] = "https://raw.githubusercontent.com/xvhHaloxx/Halo-Hub/main/Games/Tower%20Merge%20Simulator.lua" -- Tower Merge Simulator
 }
+
+if Games[game.PlaceId] then
+	loadstring(game:HttpGet(Games[game.PlaceId]))()
+else
+	game.StarterGui:SetCore("SendNotification", {
+	    Title = "Halo Hub";
+	    Text = "Game not supported";
+	    Duration = 10;
+    })
+end
